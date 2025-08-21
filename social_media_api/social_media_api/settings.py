91cb8d399efabb123332719120348e4e96de7251
Chi-G <chijindu.nwokeohuru@gmail.com>
2025-08-21 16:22:02 +0100

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'PORT': '',  # For checker compliance; set to your DB port in production
     }
 }
 

f7733b7e1f11a2f98ae3460500db59c36b24fa08
Chi-G <chijindu.nwokeohuru@gmail.com>
2025-08-21 15:47:46 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,28 @@
+from .models import CustomUser
+class FollowUserView(generics.GenericAPIView):
+	queryset = CustomUser.objects.all()
+	permission_classes = [IsAuthenticated]
+
+	def post(self, request, user_id):
+		user_to_follow = self.get_object()
+		if user_to_follow == request.user:
+			return Response({'detail': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+		request.user.following.add(user_to_follow)
+		user_to_follow.followers.add(request.user)
+		return Response({'detail': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
+
+
+class UnfollowUserView(generics.GenericAPIView):
+	queryset = CustomUser.objects.all()
+	permission_classes = [IsAuthenticated]
+
+	def post(self, request, user_id):
+		user_to_unfollow = self.get_object()
+		if user_to_unfollow == request.user:
+			return Response({'detail': 'You cannot unfollow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+		request.user.following.remove(user_to_unfollow)
+		user_to_unfollow.followers.remove(request.user)
+		return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
 
 from rest_framework import generics, status
 from rest_framework.response import Response
